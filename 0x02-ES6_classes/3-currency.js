7e88606d73c99a4e56a06f187910920d11563754
mesfin2121 <medmesfin20@gmail.com>
2023-06-29 20:38:11 +0000

    Task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,37 @@
+export default class Currency {
+  // Currency constructor
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  // getter and setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must a string');
+    }
+    this._code = value;
+  }
+
+  // getter and setter for name
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
